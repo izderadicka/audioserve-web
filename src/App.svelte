33d0356d74fa82f8f04b56f859749ff8949eabcc
Ivan <ivan.zderadicka@gmail.com>
2021-12-25 11:33:16 +0100

    Initial state

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import Login from './components/Login.svelte';
+	
+	import {apiConfig, isAuthenticated} from './state/stores';
+	import {onMount} from 'svelte';
+
+	const API_BASE_URL = "http://localhost:3000";
+	apiConfig.set({
+		basePath:API_BASE_URL
+	});
+
+	onMount(async () => {
+		
+	})
+</script>
+
+
+<main class="container">
+
+{#if ! $isAuthenticated}
+	<Login/>
+{:else}
+	<nav class="nav-bar">
+		<ul>
+			<li><a href="/">AS</a></li>
+			<li></li>
+			<li>[Menu]</li>
+		</ul>
+	</nav>
+	<div class="browser">
+		<div class="breadcrumb">[home/neco/neco]</div>
+		<div class="browser">
+
+		</div>
+	</div>
+	<div class="player">
+		[player]
+	</div>
+{/if}
+
+</main>
+	
+
+
+<style>
+	
+</style>
---
+++ src/components/Login.svelte
@@ -0,0 +1,27 @@
+<script lang=ts>
+
+    import {AuthenticationApiFp} from '../api';
+import { apiConfig } from '../state/stores';
+
+    let sharedSecret:string;
+    let playbackGroup:string;
+
+    async function login() {
+        console.log("Initiating client login");
+        if (sharedSecret) {
+            //const reply =  AuthenticationApiFp({});
+        }
+    }
+
+</script>
+
+<div class="login">
+    <h1>Audioserve Login</h1>
+    <form on:submit|preventDefault="{login}">
+        <label for="shared-secret">Shared Secret</label>
+        <input type="password" name="shared-secret" bind:value="{sharedSecret}">
+        <label for="playback-group">Playback Group</label>
+        <input type="text" name="playback-group" bind:value="{playbackGroup}">
+        <button id="login-button">Log in</button>
+    </form>
+</div>
