43ca437a0a7a4b73ff65d11b9975fb42bf1ee313
Ivan <ivan.zderadicka@gmail.com>
2021-12-28 09:53:39 +0100

    Basic menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,72 +1,67 @@
 <script lang="ts">
-	import '@picocss/pico';
-	import Login from './components/Login.svelte';
-	
-	import {apiConfig, colApi, isAuthenticated} from './state/stores';
-	import {onMount} from 'svelte';
-import { CollectionsApi, Configuration } from './client';
+  import "@picocss/pico";
+  import Login from "./components/Login.svelte";
+  import Menu from "./components/Menu.svelte";
 
+  import { apiConfig, colApi, isAuthenticated } from "./state/stores";
+  import { onMount } from "svelte";
+  import { CollectionsApi, Configuration } from "./client";
 
-	const API_BASE_URL = "http://localhost:3000";
-	apiConfig.set(new Configuration({
-		basePath: API_BASE_URL,
-		credentials: 'include',
-	}));
+  const API_BASE_URL = "http://localhost:3000";
+  apiConfig.set(
+    new Configuration({
+      basePath: API_BASE_URL,
+      credentials: "include",
+    })
+  );
 
-	async function loadCollections() {
-		const collections = await $colApi.collectionsGet();
-		console.debug("Got collections list", collections);
-		const transcodings = await $colApi.transcodingsGet();
-		console.debug("Got transcodings list", transcodings);
-	}
+  async function loadCollections() {
+    const collections = await $colApi.collectionsGet();
+    console.debug("Got collections list", collections);
+    const transcodings = await $colApi.transcodingsGet();
+    console.debug("Got transcodings list", transcodings);
+  }
 
-	
+  function actOnMenu(menuEvt) {
+	  const menuSelection: string = menuEvt.detail;
+	  console.log("Menu selected", menuSelection)
+  }
 
-	onMount(async () => {
-		
-		try {
-			await loadCollections();
-		} catch (e) {
-			console.error("Error loading initial lists",typeof e, e);
-			$isAuthenticated = false;
-			// try to load again after authentication
-			const unsubsribe = isAuthenticated.subscribe(async ok => { if (ok) {
-				await loadCollections();
-
-			}})
-		}
-		
-	})
+  onMount(async () => {
+    try {
+      await loadCollections();
+    } catch (e) {
+      console.error("Error loading initial lists", typeof e, e);
+      $isAuthenticated = false;
+      // try to load again after authentication
+      const unsubsribe = isAuthenticated.subscribe(async (ok) => {
+        if (ok) {
+          await loadCollections();
+        }
+      });
+    }
+  });
 </script>
 
-
 <main class="container">
-
-{#if ! $isAuthenticated}
-	<Login/>
-{:else}
-	<nav class="nav-bar">
-		<ul>
-			<li><a href="/">AS</a></li>
-			<li></li>
-			<li>[Menu]</li>
-		</ul>
-	</nav>
-	<div class="browser">
-		<div class="breadcrumb">[home/neco/neco]</div>
-		<div class="browser">
-
-		</div>
-	</div>
-	<div class="player">
-		[player]
-	</div>
-{/if}
-
+  {#if !$isAuthenticated}
+    <Login />
+  {:else}
+    <nav class="nav-bar">
+      <ul>
+        <li><h4><a href="/">AS</a></h4></li>
+      </ul>
+	  <ul>
+        <li>[Menu] <Menu on:menu="{actOnMenu}"/></li>
+      </ul>
+    </nav>
+    <div class="browser">
+      <div class="breadcrumb">[home/neco/neco]</div>
+      <div class="browser" />
+    </div>
+    <div class="player">[player]</div>
+  {/if}
 </main>
-	
-
 
 <style>
-	
 </style>
---
+++ src/components/Menu.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  function menuClick(evt) {
+    const item = evt.target.dataset.menu;
+    dispatch("menu", item)
+  }
+</script>
+
+<aside>
+  <nav>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <ul>
+      <li>
+        <a href="#" data-menu="logout" on:click|preventDefault={menuClick}>Logout</a>
+      </li>
+      <li>
+        <a href="#" data-menu="test1" on:click|preventDefault={menuClick}>Test1</a>
+      </li>
+    </ul>
+  </nav>
+</aside>
