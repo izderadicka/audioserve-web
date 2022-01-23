8421b780a84c98d6166d0c069b778bc7664b5e45
Ivan <ivan.zderadicka@gmail.com>
2022-01-23 19:22:30 +0100

    Change menu icon

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -3,6 +3,7 @@
   import { createEventDispatcher } from "svelte";
   import type { Transcoding } from "../client";
   import { selectedTranscoding, transcodings } from "../state/stores";
+  import Menu from "svelte-material-icons/Menu.svelte";
   import {
     StorageKeys,
     TranscodingCode,
@@ -56,26 +57,7 @@ import { clickOutside } from "../util/dom";
     aria-label="Menu"
     bind:this={menuButton}
     id="main-menu-button"
-    ><svg
-      aria-hidden="true"
-      focusable="false"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      height="1.5rem"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      ><line x1="3" y1="12" x2="21" y2="12" /><line
-        x1="3"
-        y1="6"
-        x2="21"
-        y2="6"
-      /><line x1="3" y1="18" x2="21" y2="18" /></svg
-    ></a
-  >
+    ><Menu size="1.9rem"/></a>
   {#if menuVisible}
   <div use:clickOutside={menuButton} on:outclick={() => (menuVisible = false)}>
     <aside class="dropdown-content" style={menuVisible ? "" : "display:none"}>
