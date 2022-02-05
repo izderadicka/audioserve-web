47a2f8c44a6c7e7e3137e667d58616be6af79557
Ivan <ivan.zderadicka@gmail.com>
2022-02-05 19:58:32 +0100

    Add button for last remote playback position

--- src/components/Browser.svelte
+++ src/components/Browser.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { getContext, onDestroy, onMount } from "svelte";
-import { debug } from "svelte/internal";
-import type { Unsubscriber } from "svelte/store";
-import type { Cache } from "../cache";
+  import { debug } from "svelte/internal";
+  import type { Unsubscriber } from "svelte/store";
+  import type { Cache } from "../cache";
+  import Play from 'svelte-material-icons/Play.svelte';
 
   import type { AudioFile, Subfolder } from "../client";
   import {
@@ -16,13 +17,13 @@ import type { Cache } from "../cache";
     selectedCollection,
   } from "../state/stores";
   import { FolderType, StorageKeys } from "../types/enums";
-import { PlayItem } from "../types/play-item";
-import type { AudioFileExt } from "../types/types";
-import { splitPath, splitUrl } from "../util";
-import FileItem from "./FileItem.svelte";
-import FolderItem from "./FolderItem.svelte";
+  import { PlayItem } from "../types/play-item";
+  import type { AudioFileExt } from "../types/types";
+  import { splitPath, splitUrl } from "../util";
+  import FileItem from "./FileItem.svelte";
+  import FolderItem from "./FolderItem.svelte";
 
-  const cache: Cache = getContext('cache');
+  const cache: Cache = getContext("cache");
 
   let subfolders: Subfolder[] = [];
   let files: AudioFileExt[] = [];
@@ -33,15 +34,12 @@ import FolderItem from "./FolderItem.svelte";
       const result = await $colApi.colIdSearchGet({
         colId: $selectedCollection,
         q: query,
-        group: $group
+        group: $group,
       });
 
       subfolders = result.subfolders;
       files = [];
-
-    } catch (err) {
-
-    }
+    } catch (err) {}
   }
 
   async function loadFolder(folder: string) {
@@ -49,13 +47,16 @@ import FolderItem from "./FolderItem.svelte";
       const audioFolder = await $colApi.colIdFolderPathGet({
         colId: $selectedCollection,
         path: folder,
-        group: $group
+        group: $group,
       });
-      const cachedPaths = await cache.getCachedPaths($selectedCollection, folder);
+      const cachedPaths = await cache.getCachedPaths(
+        $selectedCollection,
+        folder
+      );
       console.debug("Cached files for this folder", cachedPaths);
       files = audioFolder.files!.map((file: AudioFileExt) => {
-        if (cachedPaths.indexOf(file.path) >=0) {
-          file.cached=true;
+        if (cachedPaths.indexOf(file.path) >= 0) {
+          file.cached = true;
         }
         return file;
       });
@@ -63,27 +64,31 @@ import FolderItem from "./FolderItem.svelte";
       localStorage.setItem(StorageKeys.LAST_FOLDER, folder);
       // restore last played file, if possible
       if (folderPath === undefined) {
-      const prevFile = localStorage.getItem(StorageKeys.LAST_FILE);
+        const prevFile = localStorage.getItem(StorageKeys.LAST_FILE);
         if (prevFile) {
-            console.debug(`Can try to play ${prevFile} in folder ${$currentFolder} in collection ${$selectedCollection}`);
-            const position = files.findIndex((f) => f.path === prevFile);
-            if (position>=0) {
-                let time:number|undefined;
-                try {
-                    time = parseFloat(localStorage.getItem(StorageKeys.LAST_POSITION));
-                } catch (e) {
-                    console.log("Invalid last position", e);
-                }
-                startPlaying(position, false, time)()
+          console.debug(
+            `Can try to play ${prevFile} in folder ${$currentFolder} in collection ${$selectedCollection}`
+          );
+          const position = files.findIndex((f) => f.path === prevFile);
+          if (position >= 0) {
+            let time: number | undefined;
+            try {
+              time = parseFloat(
+                localStorage.getItem(StorageKeys.LAST_POSITION)
+              );
+            } catch (e) {
+              console.log("Invalid last position", e);
             }
+            startPlaying(position, false, time)();
+          }
         }
-    }   
+      }
 
       folderPath = folder;
     } catch (resp) {
       console.error("Cannot load folder", resp);
       if (resp.status === 404) {
-        $currentFolder = {value:"", type: FolderType.REGULAR};
+        $currentFolder = { value: "", type: FolderType.REGULAR };
       } else if (resp.status === 401) {
         $isAuthenticated = false;
       }
@@ -91,7 +96,7 @@ import FolderItem from "./FolderItem.svelte";
   }
 
   function navigateTo(folder: string) {
-    return () => ($currentFolder = {value:folder, type:FolderType.REGULAR});
+    return () => ($currentFolder = { value: folder, type: FolderType.REGULAR });
   }
 
   function startPlaying(position: number, startPlay = true, time?: number) {
@@ -101,7 +106,7 @@ import FolderItem from "./FolderItem.svelte";
         file,
         position,
         startPlay,
-        time
+        time,
       });
       console.debug("Action to start to play: " + item.url);
       $playList = {
@@ -115,96 +120,146 @@ import FolderItem from "./FolderItem.svelte";
 
   const unsubsribe: Unsubscriber[] = [];
 
- unsubsribe.push(selectedCollection.subscribe((col) => {
-    if (col != undefined) {
-      if (folderPath === undefined) {
-        // restore last path from localStorage
-        $currentFolder = {value:localStorage.getItem(StorageKeys.LAST_FOLDER) || "", type: FolderType.REGULAR};
-      } else {
-        // go to root of other collection
-        $currentFolder = {value:"", type: FolderType.REGULAR};
-        if (folderPath === "") {
-          // TODO: fix it by having currentFolder as object
-          // have to enforce reload
-          loadFolder("");
+  unsubsribe.push(
+    selectedCollection.subscribe((col) => {
+      if (col != undefined) {
+        if (folderPath === undefined) {
+          // restore last path from localStorage
+          $currentFolder = {
+            value: localStorage.getItem(StorageKeys.LAST_FOLDER) || "",
+            type: FolderType.REGULAR,
+          };
+        } else {
+          // go to root of other collection
+          $currentFolder = { value: "", type: FolderType.REGULAR };
+          if (folderPath === "") {
+            // TODO: fix it by having currentFolder as object
+            // have to enforce reload
+            loadFolder("");
+          }
         }
+        localStorage.setItem(
+          StorageKeys.LAST_COLLECTION,
+          $selectedCollection.toString()
+        );
       }
-      localStorage.setItem(
-        StorageKeys.LAST_COLLECTION,
-        $selectedCollection.toString()
-      );
-    }
-  }));
+    })
+  );
 
   $: if ($currentFolder != undefined) {
     if ($currentFolder.type === FolderType.REGULAR) {
-        loadFolder($currentFolder.value);
+      loadFolder($currentFolder.value);
     } else if ($currentFolder.type === FolderType.SEARCH) {
-       searchFor($currentFolder.value);
+      searchFor($currentFolder.value);
     }
   }
 
- unsubsribe.push(cachedItem.subscribe((item) =>
- {
-   if (item) {
-    console.log("File cached", item);
-    // update folder
-    const {collection, path} = splitUrl(item.originalUrl);
+  unsubsribe.push(
+    cachedItem.subscribe((item) => {
+      if (item) {
+        console.log("File cached", item);
+        // update folder
+        const { collection, path } = splitUrl(item.originalUrl);
 
-    // update folder
-    if (collection === $selectedCollection) {
-      const position = files.findIndex((f) => f.path == path)
-      if (position>=0) {
-        let f = files[position];
-        f.cached = true;
-        files[position] = f;
+        // update folder
+        if (collection === $selectedCollection) {
+          const position = files.findIndex((f) => f.path == path);
+          if (position >= 0) {
+            let f = files[position];
+            f.cached = true;
+            files[position] = f;
+          }
+        }
+        // update playlist
+        const { folder, file } = splitPath(path);
+        if ($playList.collection == collection && $playList.folder == folder) {
+          playList.update((pl) => {
+            const position = pl.files.findIndex((f) => f.path == path);
+            if (position >= 0) {
+              pl.files[position].cached = true;
+            }
+            return pl;
+          });
+        }
       }
-    }
-    // update playlist 
-    const {folder, file} = splitPath(path);
-    if ($playList.collection == collection && $playList.folder == folder) {
-      playList.update((pl) => {
-
-        const position = pl.files.findIndex((f) => f.path == path)
-      if (position>=0) {
-        pl.files[position].cached = true;
-      }
-        return pl
-      })
-    }
-    
-   }
- }
- ))
+    })
+  );
 
   onMount(async () => {});
-  onDestroy(() => unsubsribe.forEach( u => u()));
+  onDestroy(() => unsubsribe.forEach((u) => u()));
 </script>
 
-{#if subfolders.length > 0}
-  <details open>
-    <summary>Subfolders</summary>
-    <ul>
-      {#each subfolders as fld}
-        <li on:click={navigateTo(fld.path)}><FolderItem subfolder="{fld}" 
-          extended="{$currentFolder.type != FolderType.REGULAR}"/></li>
-      {/each}
-    </ul>
-  </details>
-{/if}
-{#if files.length > 0}
-  <details open>
-    <summary>Files</summary>
-    <ul>
-      {#each files as file, pos}
-        <li on:click={startPlaying(pos,true, 0)}><FileItem name="{file.name}" duration="{file.meta.duration}" 
-            bitrate="{file.meta.bitrate}" position="{pos}" cached={file.cached}/></li>
-      {/each}
-    </ul>
-  </details>
-{/if}
+<div id="browser">
+  <div class="main-browser-panel">
+    {#if subfolders.length > 0}
+      <details open>
+        <summary>Subfolders</summary>
+        <ul>
+          {#each subfolders as fld}
+            <li on:click={navigateTo(fld.path)}>
+              <FolderItem
+                subfolder={fld}
+                extended={$currentFolder.type != FolderType.REGULAR}
+              />
+            </li>
+          {/each}
+        </ul>
+      </details>
+    {/if}
+    {#if files.length > 0}
+      <details open>
+        <summary>Files</summary>
+        <ul>
+          {#each files as file, pos}
+            <li on:click={startPlaying(pos, true, 0)}>
+              <FileItem
+                name={file.name}
+                duration={file.meta.duration}
+                bitrate={file.meta.bitrate}
+                position={pos}
+                cached={file.cached}
+              />
+            </li>
+          {/each}
+        </ul>
+      </details>
+    {/if}
+  </div>
+  <div class="browser-sidebar">
+    <div class="last-position">
+      <h6>Last Remote Position</h6>
+      <button><Play/> {"usak"} at {"23:23"}</button>
+    </div>
+    <details id="last-remote-position" open>
+    </details>
+  </div>
+</div>
 
 <style>
+  h6 {
+    margin-bottom: 0.5em;
+  }
+  #browser {
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+  }
+
+ .main-browser-panel {
+    width: 100%;
+    margin-right: 3em;
+  }
+
+  .browser-sidebar {
+    min-width: 360px;
+  }
+
+  @media (max-width: 770px) {
+    
+    #browser {
+        flex-direction: column;
+    }
+  }
   summary {
     font-weight: bold;
     font-size: 1.5rem;
@@ -220,6 +275,6 @@ import FolderItem from "./FolderItem.svelte";
   }
 
   ul li:hover {
-      color: var(--primary) !important;
+    color: var(--primary) !important;
   }
 </style>
