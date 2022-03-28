81c45d5798643aff685de777e3fbe85493d28419
Ivan <ivan.zderadicka@gmail.com>
2022-03-28 21:01:24 +0200

    Fix history
    TBD: scroll

--- src/util/history.ts
+++ src/util/history.ts
@@ -1,36 +1,49 @@
-import { currentFolder, selectedCollection } from "../state/stores"
-import { FolderType } from "../types/enums"
+import { currentFolder, selectedCollection } from "../state/stores";
+import { FolderType } from "../types/enums";
 
 export interface HistoryRecord {
-    collection: number
-    value: string
-    folderType: FolderType
-    scrollTo?: number
-    
-
+  collection: number;
+  value: string;
+  folderType: FolderType;
+  scrollTo?: number;
 }
 
 export class HistoryWrapper {
-    constructor(private beforeChange: ()=>void) {
-        window.addEventListener("popstate", this.onPopup);
-        
-    }
+  constructor(private beforeChange: () => void) {
+    window.addEventListener("popstate", this.onPopup);
+  }
 
-    add(rec: HistoryRecord) {
-        history.pushState(rec, "", `#${rec.folderType === FolderType.SEARCH?"search:":""}${encodeURIComponent(rec.value)}`); //TODO - add hash to URL
+  add(rec: HistoryRecord) {
+    if (
+      !history.state ||
+      rec.collection != history.state.collection ||
+      rec.value != history.state.value ||
+      rec.folderType != history.state.folderType
+    ) {
+      history.pushState(
+        rec,
+        "",
+        `#${
+          rec.folderType === FolderType.SEARCH ? "search:" : ""
+        }${encodeURIComponent(rec.value)}`
+      ); 
     }
+  }
 
-    private onPopup = (evt: PopStateEvent) => {
-        const state = evt.state as HistoryRecord;
-        this.beforeChange();
-        if (state) {
-            selectedCollection.set(state.collection);
-            currentFolder.set({type: state.folderType, value: state.value, scrollTo: state.scrollTo})
-        }
+  private onPopup = (evt: PopStateEvent) => {
+    const state = evt.state as HistoryRecord;
+    this.beforeChange();
+    if (state) {
+      selectedCollection.set(state.collection);
+      currentFolder.set({
+        type: state.folderType,
+        value: state.value,
+        scrollTo: state.scrollTo,
+      });
     }
+  };
 
-    finish() {
-        window.removeEventListener("popstate", this.onPopup)
-    }
-    
+  finish() {
+    window.removeEventListener("popstate", this.onPopup);
+  }
 }
