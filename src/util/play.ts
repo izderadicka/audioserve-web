e5b44b71d6f8222b87eb4e2cd75c3d9d696e9b09
Ivan <ivan.zderadicka@gmail.com>
2022-05-15 17:11:04 +0200

    fix autorewind

--- src/util/play.ts
+++ src/util/play.ts
@@ -5,7 +5,7 @@ export function calculateAutorewind(lastPlay: number) {
     const updatedBefore = Date.now() - lastPlay;
         if (updatedBefore < 10_000) return 0
         else if (updatedBefore < 5 * MINUTE) return 2
-        else if (updatedBefore < 30 * MINUTE) 15
-        else if (updatedBefore < YEAR) 30
-        else 0
+        else if (updatedBefore < 30 * MINUTE) return 15
+        else if (updatedBefore < YEAR) return 30
+        else return 0
 }
