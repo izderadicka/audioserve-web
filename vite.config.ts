1b426316eeacc5a4a9bd9510a1d0c3f21c8e772d
Ivan <ivan.zderadicka@gmail.com>
2025-09-21 08:25:40 +0200

    Update dependencies and bump version

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import pkg from "./package.json" assert { type: 'json' };
+import pkg from "./package.json" with { type: 'json' };
 import gitCommitId from "git-commit-id";
 
 export const COMMIT_HASH: string = gitCommitId();
 export const VERSION: string = pkg.version;
 process.env.VITE_COMMIT_HASH = COMMIT_HASH;
-process.env.VITE_COMMIT_HASH_SHORT = COMMIT_HASH.substring(0,8);
+process.env.VITE_COMMIT_HASH_SHORT = COMMIT_HASH.substring(0, 8);
 process.env.VITE_VERSION = VERSION;
 
 // https://vitejs.dev/config/
